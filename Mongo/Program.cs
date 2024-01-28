2ba3d26877951b23d0d7607977217fd53a3e80c3
Jovana <suslic.jovana@elfak.rs>
2024-01-29 00:52:44 +0100

    swagger

--- Mongo/Program.cs
+++ Mongo/Program.cs
@@ -4,7 +4,12 @@ using MongoDB.Bson;
 using System.Text.Json.Serialization;
 using Microsoft.OpenApi.Models;
 
-const string connectionUri = "mongodb+srv://acokanovic14:<password>@mongo.tdt3640.mongodb.net/?retryWrites=true&w=majority";
+
+
+const string connectionUri = "mongodb+srv://susulicj:4ucninzkXfk2cmI5@cluster0.gtz9mac.mongodb.net/";
+
+
+//const string connectionUri = "mongodb+srv://acokanovic14:<password>@mongo.tdt3640.mongodb.net/?retryWrites=true&w=majority";
 
 var settings = MongoClientSettings.FromConnectionString(connectionUri);
 
@@ -22,45 +27,43 @@ try {
   Console.WriteLine(ex);
 }
 
-var builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 
-});
 
 // Add Swashbuckle services
-builder.Services.AddEndpointsApiExplorer();  // Required for minimal APIs
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORS", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowAnyOrigin(); 
+    });
+});
+builder.Services.AddControllers();
+builder.Services.AddSingleton(x => new MongoClient(connectionUri));
+builder.Services.AddTransient<IMongoClient, MongoClient>();
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
 
 app.UseDeveloperExceptionPage();
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    });
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+}
+
 
 app.Run();
 
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
